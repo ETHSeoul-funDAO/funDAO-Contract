29f56fce0741e63f6a07bfea3ac347134b80eb45
exd0tpy <eddy@sooho.io>
2023-06-03 16:15:03 +0900

    minor fix

--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Vault.sol";
 contract VaultFactory {
-
-    using SafeERC20 for ERC20;
 
     address public owner;
 
     Vault[] public funds;
 
     uint256 public constant DENOM = 1e5;
-    uint public constant DEFAULT_THRESHOLD = 0.51e8;
-    
+    uint public constant DEFAULT_THRESHOLD = 0.5e18;
+
     modifier onlyOwner {
         require(owner == msg.sender, "only Owner");
         _;
